4103b1ab4e566f9175aee5ccc7e5c3014b215df2
Nathan E. Egge <negge@dgql.org>
2016-12-02 02:08:24 -0500

    Remove the unused od_bin_idct8x8() code.

--- src/dct.h
+++ src/dct.h
@@ -16,7 +16,6 @@ See the License for the specific language governing permissions and limitations
 #if !defined(_dct_H)
 # define _dct_H (1)
 
-void od_bin_idct8x8(short *x, int xstride, const short *y, int ystride);
 void glj_real_idct8x8(short *x, int xstride, const short *y, int ystride);
 
 #endif
